bc451460ace08b402a9878f72837394bbdc0c424
Armin-Ljajic <73130506+Armin-Ljajic@users.noreply.github.com>
2021-09-13 18:20:55 +0200

    Startup code

--- ljudio-project/src/App.vue
+++ ljudio-project/src/App.vue
@@ -1,21 +1,24 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+ <template>
+ <body>
+    <Player/>
+    <router-view/>
+ </body>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+import Player from './components/Player.vue'
+export default {
+  components: {
+    Player
+  }
 }
+</script>
+
+<style>
+  body{
+    background-image: url(https://wallpaperaccess.com/full/2142842.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    
+  }
 </style>
